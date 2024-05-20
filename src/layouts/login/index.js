8afe920618810416f8837eecfe12a6b1652bc948
Ramzi Shodmonbekov <shodmonbekov516@gmail.com>
2024-05-20 19:57:15 +0600

    add routes.js

--- src/layouts/login/index.js
+++ src/layouts/login/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function LoginLayout() {
   return (
     <div>
-      
+        
     </div>
   )
 }
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import LoginLayout from "./layouts/login";
+
+const routes = [
+    {
+        path: "/",
+        element: <LoginLayout />,
+    },
+];
+
+export default routes;  
