2496c4c5a8a0671cf50bcca6d631f629b94439c5
Ramzi Shodmonbekov <shodmonbekov516@gmail.com>
2024-05-24 20:52:20 +0600

    create sidebar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/sidebar";
 
 function DashboardLayout() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Sidebar />
+      <div class="p-4 sm:ml-64">
+        <div class="p-4  border-gray-200 rounded-lg dark:border-gray-700">
+          <div class="grid grid-cols-3 gap-4 mb-4">
+            <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+              <p class="text-2xl text-gray-400 dark:text-gray-500">asd</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
