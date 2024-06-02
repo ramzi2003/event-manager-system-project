721ce321c3573a9fee94b04e27b18db13d654f71
Ramzi Shodmonbekov <shodmonbekov516@gmail.com>
2024-06-03 03:35:57 +0600

    update sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -124,12 +124,12 @@ function Sidebar() {
               {showDropdown && (
                 <ul id="dropdown-example" className="py-2 space-y-2">
                   <li>
-                    <a
-                      href="#"
+                    <Link
+                      to="/create-event"
                       className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                     >
                       Create Event
-                    </a>
+                    </Link>
                   </li>
                   <li>
                     <a
